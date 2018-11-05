1ebff0cbceddf68f786fe3c3d633756c536684fc
greiver179 <te_hung@hotmail.com>
2018-11-05 09:52:37 +0100

    Arrow class update

--- src/com/company/Arrow.java
+++ src/com/company/Arrow.java
@@ -1,4 +1,23 @@
 package com.company;
 
 public class Arrow {
+
+
+    private void speed(){ //Speed of the arrow falling down
+
+    }
+
+    private void direction(){ //direction of the arrow
+
+    }
+
+    private boolean isItHit(){//did you miss or hit the arrow at the right time
+        return false;
+    }
+
+
+
+
+
+
 }
