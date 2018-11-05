ce09a9eaa9138c667b0f50b97b6e6c8cc1a9c20e
greiver179 <te_hung@hotmail.com>
2018-11-05 11:05:25 +0100

    Arrows, different methodes from flake

--- src/com/company/Arrow.java
+++ src/com/company/Arrow.java
@@ -1,6 +1,34 @@
 package com.company;
 
 public class Arrow {
+    private int x;
+    private int y;
+    private char symbol;
+
+    public Arrow(int x, int y, char symbol) {
+        this.x = x;
+        this.y = y;
+        this.symbol = symbol;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public void fall(){
+        y++;
+    }
+
+
+
 
 
     private void speed(){ //Speed of the arrow falling down
